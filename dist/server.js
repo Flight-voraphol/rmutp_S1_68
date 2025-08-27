c4cdec99e60aea9e36864f5a57750d96bdc1da48
Flight <voraphol-t@rmutp.ac.th>
2025-08-27 21:56:07 +0700

    Delete dist directory

--- dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_server_1 = require("@hono/node-server");
-const index_1 = require("./index");
-(0, node_server_1.serve)(index_1.default, (info) => {
-    console.log(`Server is running on ${info.port}`);
-});
