c06865e7c4f774817851661d3e22ec08e412c4d4
fahsfahg <fahsfahg@gmail.com>
2025-08-27 22:55:57 +0700

    update index

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,20 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
+//import { PrismaClient } from "../generated/prisma/Client";
+
+const prisma = new PrismaClient();
 
 const app = new Hono();
  app.get("/", (C) => C.text("hello, world"));
  app.get("/about", (C) => {
     return C.json({
-        message: "About Page"
+        message: "วรพล แต้อวง"
     });
+ });
+ app.get("/profile", async (c) => {
+     //logic
+     const profile = await prisma.profile.findMany();
+     return c.json(profile);
  });
 
 export default app;
