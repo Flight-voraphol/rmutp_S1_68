b12230f0febdca0bf09dbb8d7678fc528fc0eedf
fahsfahg <fahsfahg@gmail.com>
2025-09-03 21:41:18 +0700

    update index

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client";
+import * as bcrypt from "bcrypt";
 //import { PrismaClient } from "../generated/prisma/Client";
 
 const prisma = new PrismaClient();
@@ -21,9 +22,23 @@ app.post("/profile", async (c) => {
     //login to create a new profile 
     const body = await c.req.json();
     console.log('input of profile', body);
+    console.log('body.password(original)', body.password);
+
+    //encode password
+    const passwordHash = await bcrypt.hash(body.password, 13);
+    console.log('hash.password(after)', passwordHash);
+    body.password = passwordHash;
+    console.log('body.password(replace) ', body);
+
+    //save to db 
+    body.status= false;
+    const result = await prisma.profile.create({
+        data: body
+    })
     //output response 
     return c.json({
-        message: "create profile completed"
+        message: "create profile completed",
+        data: result 
     });
 });
-export default app;
+export default app
